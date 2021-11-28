6139245d8cbfca18ef7a963d7b979e153bf27dbb
John Williams <telestrial@gmail.com>
2021-11-28 11:37:22 -0500

    Add seeder.js and generated users.json fixture

--- seeder.js
+++ seeder.js
@@ -1,2 +1,25 @@
+// This is the seeder file to users.json. It creates a number of random users,
+// which can be set with the variable below. WARNING: Running this file will
+// rewrite users.json!
+
 const fs = require('fs');
 const faker = require('faker');
+
+const userCount = 100;
+
+const users = [];
+
+for (let i = 0; i < userCount; i++) {
+  let newUser = {
+    firstName: faker.name.findName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+  users.push(newUser);
+}
+
+fs.writeFile('users.json', JSON.stringify(users), (err) => {
+  if (err) throw err;
+  console.log('Saved!');
+});
