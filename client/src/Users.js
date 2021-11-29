68b23cf3d92ec8e4aa3aa2412ecf0a7dbd2443f6
John Williams <telestrial@gmail.com>
2021-11-29 18:32:46 -0500

    Begin users frontend

---
+++ client/src/Users.js
@@ -0,0 +1,16 @@
+import { Box, Container } from '@material-ui/core';
+import { DataGrid } from '@mui/x-data-grid';
+
+const Users = () => {
+  return (
+    <Container fixed>
+      <Box display="flex" justifyContent="center">
+        <Box mt="25%" bgcolor="white" borderRadius="5px" p="1rem">
+          lol
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default Users;
