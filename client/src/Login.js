bb1d1a9ccf2a08446e52bd40c4d52f6bf776d96c
John Williams <telestrial@gmail.com>
2021-11-29 18:33:05 -0500

    Unhook to continue development

--- client/src/Login.js
+++ client/src/Login.js
@@ -2,12 +2,16 @@ import { Container, Box } from '@material-ui/core';
 
 import LoginForm from './components/LoginForm';
 
-const Login = () => {
+const Login = ({ showUsers }) => {
+  const isAuthorized = (authStatus) => {
+    showUsers(authStatus);
+  };
+
   return (
     <Container fixed>
       <Box display="flex" justifyContent="center">
         <Box mt="25%" bgcolor="white" borderRadius="5px" p="1rem">
-          <LoginForm />
+          <LoginForm isAuthorized={isAuthorized} />
         </Box>
       </Box>
     </Container>
