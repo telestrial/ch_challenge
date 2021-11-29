ac40d93201a461205b3f6f534384f6e5ecfe27be
John Williams <telestrial@gmail.com>
2021-11-28 19:27:47 -0500

    Add Material UI reset and remove all .css styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
+import { createTheme, CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+import Login from './Login';
+
 import './App.css';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#1d3557',
+    },
+  },
+});
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Login />
+      </ThemeProvider>
+    </>
   );
 }
 
