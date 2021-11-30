06849415e79dd1aac0322d2b4634160cf53248e0
John Williams <telestrial@gmail.com>
2021-11-30 18:22:15 -0500

    Add try catch for await

--- client/src/api.js
+++ client/src/api.js
@@ -1,14 +1,18 @@
 const checkLogin = async (email, password) => {
-  const response = await fetch('/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ email, password }),
-  }).then((res) => {
-    return res.json();
-  });
-  return response;
+  try {
+    const response = await fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    }).then((res) => {
+      return res.json();
+    });
+    return response;
+  } catch (error) {
+    return { error: 'We were unable to log you in. Please try again.' };
+  }
 };
 
 module.exports.checkLogin = checkLogin;
